1fdd085456eea202dc1de57fcfccd3fe4aef10ce
Alex Mauricio camacho murillo <114542253+alexcamacho025@users.noreply.github.com>
2025-07-09 12:33:28 -0500

    Delete Semana1_CalculadoraEdad directory

--- Semana1_CalculadoraEdad/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.Write("Ingresa tu año de nacimiento: ");
-int aÑoNacimiento = int.Parse(Console.ReadLine());
-Console.Write("Ingresa el año actual: ");
-int aÑoActual = int.Parse(Console.ReadLine());
-int edad = aÑoActual - aÑoNacimiento;
-Console.WriteLine("Tu edad es: " + edad + " años.");
